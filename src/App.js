6cdce37baa70a1ae8ef558cfaffd446fa9be0c36
YashwanthChundru <yashwanth.chundru@gmail.com>
2023-05-17 17:07:13 +0000

    commit message

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+
+import LoginRoute from './components/LoginRoute'
+import HomeRoute from './components/HomeRoute'
+import NotFoundRoute from './components/NotFoundRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 import './App.css'
 
 // use the below bookshelvesList for rendering read status of book items in Bookshelves Route
@@ -25,6 +31,14 @@ const bookshelvesList = [
   },
 ]
 
-const App = () => <div>Hello World</div>
-
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginRoute} />
+      <ProtectedRoute exact path="/" component={HomeRoute} />
+      <Route path="/not-found" component={NotFoundRoute} />
+      <Redirect to="not-found" />
+    </Switch>
+  </BrowserRouter>
+)
 export default App
